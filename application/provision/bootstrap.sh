bb1a8f7566289d5fd137e1d7406e28dd8843f131
Jesse Smick <janka102@gmail.com>
2019-09-20 00:18:21 -0700

    Better instructions for starting local server
    Also got server code deployment working

--- application/provision/bootstrap.sh
+++ application/provision/bootstrap.sh
@@ -1,7 +1,8 @@
 # bootstrap a new host to get ready to be provisioned by ansible
 
-apt-get -qq update && \
-apt-get -qq full-upgrade && \
-apt-get -qq install \
+apt-get -qy update && \
+apt-get -qy full-upgrade && \
+apt-get -qy autoremove && \
+apt-get -qy install \
 aptitude \
 ansible
