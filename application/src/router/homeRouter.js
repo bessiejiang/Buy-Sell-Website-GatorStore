3658ab2337626697616893b72d67a54764a769e8
Jesse Smick <janka102@gmail.com>
2019-11-02 12:26:13 -0700

    Use item middleware for homepage

--- application/src/router/homeRouter.js
+++ application/src/router/homeRouter.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const Category = require("../controller/category");
 const Item = require("../controller/item");
 const router = express.Router();
 
-router.get("/", function(req, res) {
-  Item.find({ limit: 6, orderBy: "createdAt", orderDirection: "desc" }).then(
-    items => {
-      res.render("homepage.ejs", {
-        items
-      });
-    }
-  );
-});
+router.get(
+  "/",
+  Item.middleware({
+    limit: 6,
+    orderBy: "createdAt",
+    orderDirection: "desc"
+  }),
+  function(req, res) {
+    res.render("homepage.ejs");
+  }
+);
 
 module.exports = router;
