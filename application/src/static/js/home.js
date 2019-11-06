6669f422756001c25586c319689421bc3cc11d1d
Angelo Solitario <nglsolitario377@gmail.com>
2019-11-05 18:14:34 -0800

    modified homepage.ejs

--- application/src/static/js/home.js
+++ application/src/static/js/home.js
@@ -12,27 +12,27 @@ $(document).ready(function() {
       function(data) {
         for (var i = 0; i < data.rows.length; i++) {
           let obj = data.rows[i];
-          let img = obj.photo
-            ? `<img style="max-width: 100px" src="${obj.photo}">`
-            : "<div>NO IMAGE</div>";
-          
-          let add =
-            "<p>" +
-            obj.id +
-            " " +
-            obj.title +
-            " " +
-            obj.price +
-            " " +
-            obj.description +
-            " " +
-            "</p>";
-          let wrap =
-            "<div style='border: groove #e2dede 2px; margin: 1%'>" +
-            img +
-            add +
+          let img = `<img src="${obj.photo}" class="img-fluid card-img-top" />`;
+          let cardBody =
+            "<div class='card-body'>" +
+            "<h5 class='card-title text-center'>" +
+            `<p><a href="item/${obj.id}" target="_blank">${obj.title}</a></p>` +
+            "</h5>" +
             "</div>";
-          $(".display-container").append(wrap);
+          let add =
+            "<div class='card'>" +
+            "<h4 style='background: white;' class='position-absolute'>" +
+            `${obj.price}` +
+            "</h4>" +
+            "<div>" +
+            img;
+          "</div>" + "</div>";
+          let wrap =
+            "<div class='col-md-4 col-sm-6 col-12'>" +
+            add +
+            cardBody +
+            "</div>";
+         $(".display-container").append(wrap);
         }
       }
     );
